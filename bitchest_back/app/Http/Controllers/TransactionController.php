4d02dbeb3a1d81427ee6522c0662a72a22eb989f
Drallireeh <kenny.herillard@hotmail.fr>
2021-08-30 11:52:44 +0200

    Create controllers for currencies and transactions

---
+++ bitchest_back/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Transaction;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transactions = Transaction::all();
+
+        return response([
+            'transactions' => $transactions,
+            'message' => 'Transactions récupérées avec succès'
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $transaction = Transaction::create($request->all());
+
+        return response([
+            'transaction' => $transaction,
+            'message' => 'Transaction effectuée avec succès'
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Transaction::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::find($id);
+
+        $transaction->update($request->all());
+
+        return response([
+            'transaction' => $transaction,
+            'message' => 'Transaction mise à jour'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = Transaction::find($id);
+
+        $transaction->delete();
+
+        return response([
+            'message' => 'Transaction supprimée avec succès'
+        ], 200);
+    }
+}
