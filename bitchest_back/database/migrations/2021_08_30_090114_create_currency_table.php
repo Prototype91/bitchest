c787b8b7736879edcad74002a1d247108999bdd6
Drallireeh <kenny.herillard@hotmail.fr>
2021-08-30 11:18:46 +0200

    Create table currencies

---
+++ bitchest_back/database/migrations/2021_08_30_090114_create_currency_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('symbol');
+            $table->string('coin_id');
+            $table->float('value');
+            $table->timestamp('last_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+}
