8d7cc83426222ff21f3e336eae888be5318b451c
Drallireeh <kenny.herillard@hotmail.fr>
2021-08-31 12:04:03 +0200

    feat(back): resolve problems on foreigns transaction

--- bitchest_back/database/migrations/2021_08_30_090114_create_currency_table.php
+++ bitchest_back/database/migrations/2021_08_30_090114_create_currency_table.php
@@ -14,7 +14,7 @@ class CreateCurrencyTable extends Migration
     public function up()
     {
         Schema::create('currency', function (Blueprint $table) {
-            $table->id();
+            $table->integerIncrements("id");
             $table->string('name');
             $table->string('symbol');
             $table->string('coin_id');
