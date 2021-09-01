4c10098558a1f1ea112a088cca0171fdbeb2437f
Drallireeh <kenny.herillard@hotmail.fr>
2021-09-01 15:04:14 +0200

    feat(back): Add validations rules inside users creation

--- bitchest_client/src/components/shared/EditUserForm.vue
+++ bitchest_client/src/components/shared/EditUserForm.vue
@@ -6,12 +6,10 @@
         <label for="lastname">Nom :</label> <br />
         <input
           type="text"
-          name="lastname"
           v-model="user.lastname"
           class="form-control"
           id="lastname"
           placeholder="Nom"
-          required
           @blur="v$.$touch"
         />
         <div class="error" v-if="v$.user.lastname.$error">
@@ -21,13 +19,11 @@
       <div>
         <label for="firstname">Prénom : </label>
         <input
-          name="firstname"
           class="form-control"
           v-model="user.firstname"
           type="text"
           id="firstname"
           placeholder="Prénom"
-          required
           @blur="v$.$touch"
         />
         <div class="error" v-if="v$.user.firstname.$error">
@@ -37,9 +33,7 @@
       <div>
         <label for="mail">Mail : </label>
         <input
-          name="mail"
           class="form-control"
-          required
           v-model="user.email"
           id="mail"
           type="mail"
@@ -53,13 +47,11 @@
       <div>
         <label for="address">Adresse : </label>
         <input
-          name="address"
           class="form-control"
           v-model="user.address"
           type="text"
           id="address"
           placeholder="Lieu de résidence"
-          required
           @blur="v$.$touch"
         />
         <div class="error" v-if="v$.user.address.$error">
@@ -69,7 +61,6 @@
       <div>
         <label for="phone">Téléphone : </label>
         <input
-          name="phone"
           class="form-control"
           id="phone"
           v-model="user.phone"
@@ -84,14 +75,12 @@
       <div v-if="updatePassword == true">
         <label for="password">Mot de passe : </label>
         <input
-          name="password"
           class="form-control"
           v-model="user.password"
           type="password"
           id="password"
           placeholder="Mot de passe"
           autocomplete="off"
-          required
           @blur="v$.$touch"
         />
         <div class="error" v-if="v$.user.password.$error">
@@ -101,14 +90,12 @@
       <div v-if="updatePassword == true">
         <label for="password-confirm">Confirmer le mot de passe : </label>
         <input
-          name="password-confirm"
           class="form-control"
           v-model="user.password_confirmation"
           type="password"
           id="password-confirm"
           placeholder="Confirmation du mot de passe"
           autocomplete="off"
-          required
           @blur="v$.$touch"
         />
         <div class="error" v-if="v$.user.password_confirmation.$error">
