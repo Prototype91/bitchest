870a0cc718afcb91bd3c8111e2fb78fdce89d1cd
Drallireeh <kenny.herillard@hotmail.fr>
2021-09-02 16:03:14 +0200

    feat(client): We can now buy cryptos

--- bitchest_client/src/views/public/MarketPlace.vue
+++ bitchest_client/src/views/public/MarketPlace.vue
@@ -8,15 +8,20 @@
           <button class="btn btn-secondary">Vendre</button>
         </div>
 
-        <div class="inputs">
+        <form @submit.prevent="startTransfert" class="inputs">
           <div class="input-euro">
             <label for="acheter">Dépenser</label>
             <div class="input-ctn">
               <div>
-                <input id="acheter" type="number" v-model="exchange_value" @keyup="calculate">
+                <input
+                  id="acheter"
+                  type="number"
+                  v-model="exchange_value"
+                  @keyup="calculate"
+                />
               </div>
               <div class="select-ctn">
-                <img src="" alt="Symbole de la monnaie utilisée">
+                <img src="" alt="Symbole de la monnaie utilisée" />
                 <span>EUR</span>
               </div>
             </div>
@@ -25,17 +30,29 @@
             <label for="vendre">Recevoir</label>
             <div class="input-ctn">
               <div>
-                <input id="vendre" type="text" v-model="this.crypto_value">
+                <input id="vendre" type="text" v-model="this.crypto_amount" />
               </div>
               <div class="select-ctn">
-                <img src="" alt="icone de la cryptomonnaie choisie">
-                <select name="crypto-select" id="crypto-select" v-model="currencySelected" @change="setCurrentCurrency">
-                  <option v-for="(currency, index) in this.cryptoCurrenciesData" :key=index :value=currency.id>{{currency.id}}</option>
+                <img src="" alt="icone de la cryptomonnaie choisie" />
+                <select
+                  name="crypto-select"
+                  id="crypto-select"
+                  v-model="currencySelected"
+                  @change="setCurrentCurrency"
+                >
+                  <option
+                    v-for="(currency, index) in this.cryptoCurrenciesData"
+                    :key="index"
+                    :value="currency.id"
+                  >
+                    {{ currency.id }}
+                  </option>
                 </select>
               </div>
             </div>
           </div>
-        </div>
+          <button class="btn btn-success" type="submit">Conclure</button>
+        </form>
       </section>
     </div>
   </main>
@@ -43,34 +60,74 @@
 
 <script>
 import Navigation from "../../components/shared/Navigation.vue";
-import cryptoCurrenciesMapper from '../../services/cryptoCurrencies/cryptoCurrencies.mapper';
-import cryptoCurrenciesService from '../../services/cryptoCurrencies/cryptoCurrencies.service';
+import cryptoCurrenciesMapper from "../../services/cryptoCurrencies/cryptoCurrencies.mapper";
+import cryptoCurrenciesService from "../../services/cryptoCurrencies/cryptoCurrencies.service";
+import localStorageService from "../../services/localStorage/localStorage.service";
+import transactionsService from "../../services/transactions/transactions.service";
+
 export default {
   name: "MarketPlace",
   components: { Navigation },
   data() {
     return {
-      currencySelected: 'bitcoin',
+      userData: null,
+      currencySelected: "bitcoin",
+      currencySymbol: "",
       currencyPrice: 0,
       exchange_value: 0,
-      crypto_value: 0,
-      cryptoCurrenciesData: []
+      crypto_amount: 0,
+      cryptoCurrenciesData: [],
     };
   },
   mounted() {
-    cryptoCurrenciesService.getCryptoCurrencies().then(response => {
-      this.cryptoCurrenciesData = cryptoCurrenciesMapper.mapCryptoCurrencies(response);
-      console.log(this.cryptoCurrenciesData)
-    })
+    this.userData = localStorageService.getUserLocalStorage();
+
+    cryptoCurrenciesService.getCryptoCurrencies().then((response) => {
+      this.cryptoCurrenciesData =
+        cryptoCurrenciesMapper.mapCryptoCurrencies(response);
+      console.log(this.cryptoCurrenciesData);
+    });
   },
   methods: {
     calculate() {
-      this.crypto_value = this.exchange_value / this.currencyPrice;
+      this.crypto_amount = this.exchange_value / this.currencyPrice;
     },
     setCurrentCurrency() {
-      const currencyData = this.cryptoCurrenciesData.filter(currency => currency.id === this.currencySelected);
+      console.log("MAIS NIQUE TOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+      const currencyData = this.cryptoCurrenciesData.filter(
+        (currency) => currency.id === this.currencySelected
+      );
+      this.currencySymbol = currencyData[0].symbol;
       this.currencyPrice = currencyData[0].current_price;
-    }
+    },
+    startTransfert() {
+      let currency_id;
+      let data;
+      transactionsService.getCurrencies().then((response) => {
+        currency_id = response.data.currencies.filter(
+          (currency) => currency.coin_id === this.currencySelected
+        )[0].id;
+
+        data = {
+          currency_id: currency_id,
+          currency_value: this.crypto_amount,
+          user_id: this.userData.id,
+          amount: this.exchange_value,
+          type: true,
+          name: this.currencySelected,
+          symbol: this.currencySymbol,
+        };
+        console.log("data : ", data);
+        transactionsService
+          .addNewUserTransaction(data)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    },
   },
 };
 </script>
@@ -96,66 +153,64 @@ export default {
   margin-bottom: 25px;
 }
 
-
-
 .input-ctn {
   box-sizing: border-box;
-    margin: 0;
-    min-width: 0;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-flex: 1;
-    -ms-flex: 1;
-    flex: 1;
-    margin-top: 10px;
-    -webkit-align-items: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
+  margin: 0;
+  min-width: 0;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  margin-top: 10px;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
 }
 
 .input-ctn input {
-      box-sizing: border-box;
-    margin: 0;
-    min-width: 0;
-    width: 100%;
-    height: 24px;
-    color: #1E2329;
-    font-size: 20px;
-    outline: none;
-    background-color: #FAFAFA;
-    border: 0 none;
-    line-height: 21px;
-    font-family: inherit;
-    font-weight: mediumm;
-        color: rgb(208, 202, 192);
-    outline-color: initial;
-    background-color: rgb(39, 40, 39);
-    border-color: initial;
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  width: 100%;
+  height: 24px;
+  color: #1e2329;
+  font-size: 20px;
+  outline: none;
+  background-color: #fafafa;
+  border: 0 none;
+  line-height: 21px;
+  font-family: inherit;
+  font-weight: mediumm;
+  color: rgb(208, 202, 192);
+  outline-color: initial;
+  background-color: rgb(39, 40, 39);
+  border-color: initial;
 }
 
 .select-ctn {
   box-sizing: border-box;
-    margin: 0;
-    min-width: 0;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    background-color: darkslategrey;
-    border-radius: 50px;
-    padding: 8px;
-    -webkit-align-items: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    cursor: pointer;
-    width: 116px;
+  margin: 0;
+  min-width: 0;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  background-color: darkslategrey;
+  border-radius: 50px;
+  padding: 8px;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 116px;
 }
 </style>
