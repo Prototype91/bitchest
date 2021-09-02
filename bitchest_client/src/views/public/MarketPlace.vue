d089d9688bbb70cce0d53e9544ea2e7647aff20b
Drallireeh <kenny.herillard@hotmail.fr>
2021-09-02 12:18:59 +0200

    feat(client): Begin Buy/Sold page

--- bitchest_client/src/views/public/MarketPlace.vue
+++ bitchest_client/src/views/public/MarketPlace.vue
@@ -2,11 +2,29 @@
   <main>
     <Navigation />
     <div class="market-ctn">
-      <section class="buy">
-        <h2>Acheter</h2>
-      </section>
       <section class="sell">
-        <h2>Vendre</h2>
+        <div class="btn-ctn">
+          <button class="btn btn-primary">Acheter</button>
+          <button class="btn btn-secondary">Vendre</button>
+        </div>
+
+        <div class="inputs">
+          <div class="input-euro">
+            <label for="acheter">Dépenser</label>
+            <input id="acheter" type="text">
+          </div>
+          <div class="input-crypto">
+            <label for="vendre">Recevoir</label>
+            <div class="input-ctn">
+              <div>
+                <input id="vendre" type="text">
+              </div>
+              <div class="select-ctn">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   </main>
@@ -21,16 +39,77 @@ export default {
 </script>
 
 <style scoped>
-.buy,
-.sell {
-  width: 50%;
-  text-align: center;
-}
-
 .market-ctn {
   display: flex;
   justify-content: center;
-  width: calc(100% - 120px);
-  margin-left: 130px;
+}
+
+.market-ctn section {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.market-ctn .btn-ctn {
+  width: 250px;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+
+
+.input-ctn {
+  box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    margin-top: 10px;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+.input-ctn input {
+  box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+}
+
+.select-ctn {
+  box-sizing: border-box;
+    margin: 0;
+    min-width: 0;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    background-color: #FFFFFF;
+    border-radius: 50px;
+    padding: 8px;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    cursor: pointer;
+    width: 116px;
 }
 </style>
