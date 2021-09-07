c4abc054c6f5d73b9865bd3fcd71a5954b7cc75d
Drallireeh <kenny.herillard@hotmail.fr>
2021-09-07 09:53:00 +0200

    feat(client): Page not found style/work

--- bitchest_client/src/views/shared/PageNotFound.vue
+++ bitchest_client/src/views/shared/PageNotFound.vue
@@ -1,13 +1,31 @@
 <template>
+  <main>
     <h1>Page not found ...</h1>
+    <button class="btn btn-primary" @click="retour()">Retour</button>
+  </main>
 </template>
 
 <script>
 export default {
-    name: 'PageNotFound'
-}
+  name: "PageNotFound",
+  methods: {
+    retour() {
+      history.back();
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 
+h1 {
+  font-size: 3rem;
+  margin-bottom: 20px;
+}
 </style>
